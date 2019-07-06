51a890958bee49f477c2dd9ea3ff7a37d84a9433
anshul3899 <anshulyadav1911@gmail.com>
2019-07-06 16:56:05 +0530

    clipped loss and network parameters

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ np.random.seed(786)
 
 
 class Net:
-    def __init__(self, input_dim, latent_dim, output_dim,  a=1, b=10**(-4), alpha=0.8, gamma=0.2):
+    def __init__(self, input_dim, latent_dim, output_dim,  a=1e-6, b=1e-2, alpha=0.602, gamma=0.101):
         self.input_dim = input_dim
         self.latent_dim = latent_dim
         self.output_dim = output_dim
@@ -15,60 +15,94 @@ class Net:
         self.weights = []
         self.biases = []
         self.weights.append(np.random.randn(self.input_dim, self.latent_dim))
+        self.weights.append(np.random.randn(self.latent_dim, self.latent_dim))
+        self.weights.append(np.random.randn(self.latent_dim, self.latent_dim))
         self.weights.append(np.random.randn(self.latent_dim, self.output_dim))
-        self.biases.append(np.random.randn(self.input_dim))
-        self.biases.append(np.random.randn(self.output_dim))
+        
+        # self.biases.append(np.zeros([1,self.latent_dim]))
+        # self.biases.append(np.zeros([1,self.latent_dim]))
+        # self.biases.append(np.zeros([1,self.latent_dim]))
+        # self.biases.append(np.zeros([1,self.output_dim]))
 
-    def train(self, inputs, targets, t_max=200):
+
+    def train(self, inputs, targets, t_max=100):
         for l in range(len(self.weights)):
             w_p = np.copy(self.weights)
             w_m = np.copy(self.weights)
-            b_p = np.copy(self.biases)
-            b_m = np.copy(self.biases)
+            # b_p = self.biases.copy()
+            # b_m = self.biases.copy()
             for t in range(1, t_max):
-                a_t = self.a / (1+t+500)**self.alpha
+                saved_weights = np.copy(self.weights[l])
+                preds = self.forward(inputs, self.weights)
+                loss_old = self.loss(preds, targets)
+
+                a_t = self.a / (1+t+50000)**self.alpha
                 b_t = self.b / (1+t)**self.gamma
                 delta = np.random.binomial(1, p=0.5, size=self.weights[l].shape) * 2. - 1
-                delta2 = np.random.binomial(1, p=0.5, size=self.biases[l].shape) * 2. - 1
+                delta2 = np.random.binomial(1, p=0.5, size=self.weights[l].shape) * 2. - 1
                 # perturb weights in plus directions
                 w_p[l] = w_p[l] + b_t * delta
-                b_p[l] = b_p[l] + b_t * delta2
+                # b_p[l] = b_p[l] + b_t * delta2
                 # compute predictions according to W_p and then compute loss using perturbed weight
-                preds = self.forward(inputs, w_p, b_p)
+                preds = self.forward(inputs, w_p)
                 loss_p = self.loss(preds, targets)
                 # perturb weights in minus directions
                 w_m[l] = w_m[l] - b_t * delta
-                b_m[l] = b_m[l] - b_t * delta2
+                # b_m[l] = b_m[l] - b_t * delta2
                 # compute predictions according to W_m and then compute loss using perturbed weight
-                preds = self.forward(inputs, w_m, b_m)
+                preds = self.forward(inputs, w_m)
                 loss_m = self.loss(preds, targets)
                 # Compute approximation of the gradient
                 g_hat = (loss_p - loss_m) / (2 * b_t * delta)
-                g_hat2 = (loss_p - loss_m) / (2 * b_t * delta2)
+                # g_hat2 = (loss_p - loss_m) / (2 * b_t * delta2)
+                # For updating weights
                 breaking_bad = True
                 clip_max = np.ones(self.weights[l].shape)*5
                 clip_min = np.ones(self.weights[l].shape)*(-5)
-                w_new = self.weights[l]
+                this_ak = (self.weights[l]*0 + 1)*a_t
+                W_new = self.weights[l]
                 while breaking_bad:
-                    if(w_new - a_t * g_hat < clip_max).all()  or (w_new - a_t * g_hat > clip_min).all():
-                        self.weights[l] = self.weights[l] - a_t * g_hat
+                    out_of_bounds = np.where ( np.logical_or ( \
+                        W_new - this_ak*g_hat > clip_max, 
+                        W_new - this_ak*g_hat < clip_min ) )[0]
+                    W_new = self.weights[l] - this_ak*g_hat
+                    if len ( out_of_bounds ) == 0:
+                        self.weights[l] = self.weights[l] - this_ak*g_hat
                         breaking_bad = False
                     else:
-                        a_t = a_t / 2
-                    w_new = self.weights[l] - a_t * g_hat
-                # print("Logging Gradient descent update", np.max(a_t * g_hat))                                    
-                self.biases[l] = self.biases[l] - a_t * g_hat2
-        # print("Logging weights:", self.weights)
-        # print("Logging Gradient descent update", np.max(a_t * g_hat))
-        return self.forward(inputs, self.weights, self.biases)
+                        this_ak[out_of_bounds] = this_ak[out_of_bounds]/2.
+                preds = self.forward(inputs, self.weights)
+                loss_new = self.loss (preds, targets)
+                
+                if np.abs ( loss_new - loss_old ) > 5:
+                    self.weights[l] = saved_weights
+                    continue
+                else:
+                    loss_old = loss_new
+
+        return self.forward(inputs, self.weights)
 
     @staticmethod
-    def forward(inputs, w, b):
-        out = np.dot(w[0].T, inputs) + b[0]
+    def forward(inputs, w):
+        out = inputs.dot(w[0])
         out = np.tanh(out)
-        out = np.dot(w[1].T, out) + b[1]
+        out = out.dot(w[1]) 
+        out = np.tanh(out)
+        out = out.dot(w[2]) 
+        out = np.tanh(out)
+        out = out.dot(w[3]) 
         return out
 
     @staticmethod
     def loss(preds, targets):
         return np.sum((preds-targets)**2)
+
+    def getPreds(self, inputs):
+        out = inputs.dot(self.weights[0])
+        out = np.tanh(out)
+        out = out.dot(self.weights[1])
+        out = np.tanh(out)
+        out = out.dot(self.weights[2])
+        out= np.tanh(out)
+        out = out.dot(self.weights[3])
+        return out
--- train.py
+++ train.py
@@ -3,9 +3,9 @@ import numpy as np
 from model import Net
 
 # Hyperparams
-input_dim = 15
+input_dim = 1
 latent_dim = 100
-output_dim = 15
+output_dim = 1
 
 inputs = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
                    [9.779], [6.182], [7.59], [2.167], [7.042],
@@ -15,10 +15,12 @@ targets = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
                     [3.366], [2.596], [2.53], [1.221], [2.827], 
                     [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
 
-num_epochs = 5000
+num_epochs = 1000
 model = Net(input_dim, latent_dim, output_dim)
 
 for epoch in range(num_epochs):
     preds = model.train(inputs, targets)
-    if epoch%100==0:
+    if epoch%1==0:
         print(epoch, "\tMSELoss is: ", np.sum((preds - targets)**2))
+
+print("===>>> Preds are: \n{}\n===>>> Targets are: \n{}".format(model.getPreds(inputs), targets))
